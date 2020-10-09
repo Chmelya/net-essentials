2d747092c6b4afbe5a7d6069d16ab2934ed9f969
Chmelya <chmel.dmitriy@mail.ru>
2020-10-09 15:48:43 +0300

    completed task5

---
+++ task5/Tree/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tree
+{
+    public class Node
+    {
+        public Node(Node left, Node right, string val)
+            => (Left, Right, Val) = (left, right, val);
+
+        public Node Left { get; }
+        public Node Right { get; }
+        public string Val { get; }
+    }
+}
