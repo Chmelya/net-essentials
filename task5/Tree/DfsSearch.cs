46e74825f4d38ceed1fea50db3c188b897ae9820
Chmelya <chmel.dmitriy@mail.ru>
2020-10-09 16:18:03 +0300

    finaly

---
+++ task5/Tree/DfsSearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    public class DfsSearch
+    {
+        int count;
+        Dictionary<int, Node> byNum;
+        Dictionary<string, Node> byName;
+
+        public DfsSearch(Node tree)
+        {
+            byName = new Dictionary<string, Node>();
+            byNum = new Dictionary<int, Node>();
+            count = 0;
+            Find(tree);
+        }
+
+        void Find(Node n)
+        {
+            byNum.Add(count, n);
+            byName.Add(n.Val, n);
+
+            count++;
+
+            if (n.Left != null) Find(n.Left);
+            if (n.Right != null) Find(n.Right);
+        }
+
+
+        public Node this[int index]
+        {
+            get => byNum[index];
+        }
+
+        public Node this[string val]
+        {
+
+            get => byName[val];
+
+        }
+    }
+}
