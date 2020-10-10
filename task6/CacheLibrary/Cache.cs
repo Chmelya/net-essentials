a6b0a9dadefcde2acdab3f59eb6b789016b950a8
Chmelya <chmel.dmitriy@mail.ru>
2020-10-10 15:32:31 +0300

    finaly task6

---
+++ task6/CacheLibrary/Cache.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CacheLibrary
+{
+    public class Cache<TKey, TValue>: ICache<TKey, TValue>
+        where TKey : struct
+        where TValue : class
+    {
+        Dictionary<TKey, TValue> cacheVal;
+        Dictionary<TKey, DateTime> cacheTime;
+
+        public Cache()
+        {
+            cacheVal = new Dictionary<TKey, TValue>();
+            cacheTime = new Dictionary<TKey, DateTime>();
+        }
+
+        public void AddOrUpdate(TKey key, TValue value, DateTime expiresOn)
+        {
+            if (cacheVal.ContainsKey(key))
+            {
+                cacheVal[key] = value;
+                cacheTime[key] = expiresOn;
+            }
+            else
+            {
+                cacheVal.Add(key, value);
+                cacheTime.Add(key, expiresOn);
+            }
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (cacheVal.ContainsKey(key))
+                if (cacheVal[key] == null)
+                    return null;
+
+            if (DateTime.Now > cacheTime[key])
+                return null;
+
+            return cacheVal[key];
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (!cacheVal.ContainsKey(key))
+                return false;
+
+            cacheVal.Remove(key);
+            cacheTime.Remove(key);
+
+            return true;
+        }
+    }
+}
---
+++ task6/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CacheLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cache<double, string> cache = new Cache<double, string>();
+
+            cache.AddOrUpdate(1, "hello", DateTime.Now.AddSeconds(5));
+
+            cache.AddOrUpdate(2, "world", DateTime.Now.AddHours(2));
+
+            System.Threading.Thread.Sleep(7000);
+
+            var getA = cache.Get(1); //null
+            var getB = cache.Get(2); //world
+            cache.Remove(2);
+
+            cache.AddOrUpdate(1, "!", DateTime.Now.AddDays(1));
+            var getC = cache.Get(1); // !
+            Console.WriteLine(getC);
+        }
+    }
+}
