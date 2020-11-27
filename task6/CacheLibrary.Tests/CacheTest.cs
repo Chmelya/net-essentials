54b207d6d2320a24aa06b0a5ae9ca8d4e2aec84d
Chmelya <chmel.dmitriy@mail.ru>
2020-11-27 22:03:58 +0300

    tests fixed

--- task6/CacheLibrary.Tests/CacheTest.cs
+++ task6/CacheLibrary.Tests/CacheTest.cs
@@ -1,10 +1,11 @@
 using NUnit.Framework;
 using CacheLibrary;
 using System;
+using System.Threading;
 
 namespace CacheLibrary.Tests
 {
-    public class CacheTest
+    public class CacheTests
     {
         ICache<double, string> cache;
          
@@ -15,7 +16,7 @@ namespace CacheLibrary.Tests
         }
 
         [Test]
-        public void Add_1_hello()
+        public void AddOrUpdate_AddedItemDoesNotExistInCache_ShouldAddNewItem()
         {
             cache.AddOrUpdate(1, "hello", DateTime.Now.AddSeconds(5));
 
@@ -23,7 +24,7 @@ namespace CacheLibrary.Tests
         }
 
         [Test]
-        public void Update_1_hello()
+        public void AddOrUpdate_AddedItemExistInCache_ShouldUpdateItem()
         {
             cache.AddOrUpdate(2, "hello", DateTime.Now.AddSeconds(5));
             cache.AddOrUpdate(2, "hell", DateTime.Now.AddSeconds(5));
@@ -32,7 +33,7 @@ namespace CacheLibrary.Tests
         }
 
         [Test]
-        public void Remove_returnFalse()
+        public void RemoveItem_ItemExistInCache_ShouldReturnFalse()
         {
             cache.AddOrUpdate(3, "world", DateTime.Now.AddSeconds(5));
 
@@ -40,7 +41,7 @@ namespace CacheLibrary.Tests
         }
 
         [Test]
-        public void Remove_returnTrue()
+        public void RemoveItem_ItemExistInCache_ShouldReturnTrue()
         {
             cache.AddOrUpdate(5, "world", DateTime.Now.AddSeconds(5));
 
@@ -48,11 +49,21 @@ namespace CacheLibrary.Tests
         }
 
         [Test]
-        public void Get()
+        public void GetItem_ItemExistInCache_ShouldReturnItem()
         {
             cache.AddOrUpdate(6, "six", DateTime.Now.AddSeconds(5));
 
             Assert.AreEqual("six", cache.Get(6));
+        }
+
+        [Test]
+        public void GetItem_ItemExpired_ShouldNotReturnItem()
+        {
+            cache.AddOrUpdate(7, "seven", DateTime.Now.AddSeconds(1));
+
+            Thread.Sleep(1200);
+
+            Assert.AreEqual(null, cache.Get(7));
         }
     }
 }
