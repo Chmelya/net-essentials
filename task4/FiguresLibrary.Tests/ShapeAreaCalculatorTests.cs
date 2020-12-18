0328827578a74979185280e2003955cee2204fb9
Chmelya <chmel.dmitriy@mail.ru>
2020-12-19 01:14:41 +0300

    last fix

--- task4/FiguresLibrary.Tests/ShapeAreaCalculatorTests.cs
+++ task4/FiguresLibrary.Tests/ShapeAreaCalculatorTests.cs
@@ -9,25 +9,10 @@ namespace FiguresLibrary.Tests
     {
         private IShapeAreaCalculator areaCalculator;
 
-        private static object[] shapesGetAreaCases =
-        {
-            new object[] { new Triangle(2, 3), 3 },
-            new object[] { new Triangle(4, 5), 10 },
-            new object[] { new Rectangle(4, 2), 8 },
-            new object[] { new Rectangle(5, 6), 30 },
-            new object[] { new Circle(2), Math.PI }
-        };
-
         [SetUp]
         public void Setup()
         {
             areaCalculator = new ShapeAreaCalculator();
-        }
-
-        [TestCaseSource("shapesGetAreaCases")]
-        public void GetArea_ProvidedDifferentShapeTypes_ReturnsCorrectArea(Shape shape, double expectedArea)
-        {
-            Assert.AreEqual(expectedArea, shape.GetArea());
         }
 
         [Test]
--- task4/FiguresLibrary.Tests/ShapesTests.cs
+++ task4/FiguresLibrary.Tests/ShapesTests.cs
@@ -5,14 +5,29 @@ using System;
 
 namespace FiguresLibrary.Tests
 {
-    public class ShapesNameTests
+    public class ShapesTests
     {
+        private static object[] shapesGetAreaCases =
+        {
+            new object[] { new Triangle(2, 3), 3 },
+            new object[] { new Triangle(4, 5), 10 },
+            new object[] { new Rectangle(4, 2), 8 },
+            new object[] { new Rectangle(5, 6), 30 },
+            new object[] { new Circle(2), Math.PI }
+        };
+
         private static object[] shapesNameCases =
         {
             new object[] { new Triangle(1, 4), "Triangle" },
             new object[] { new Rectangle(4, 5), "Rectangle" },
             new object[] { new Circle(2), "Circle" }
         };
+
+        [TestCaseSource("shapesGetAreaCases")]
+        public void GetArea_ProvidedDifferentShapeTypes_ReturnsCorrectArea(Shape shape, double expectedArea)
+        {
+            Assert.AreEqual(expectedArea, shape.GetArea());
+        }
 
         [TestCaseSource("shapesNameCases")]
         public void GetName_ProvidedDefferentShapeTypes_ReturnsCorrectName(Shape shape, string expectedName)
